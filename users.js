9939cce0d9b36e3424720018e0be318d53de9cb3
Benjamin Gnewuch <bgnewuch@yahoo.com>
2016-03-30 10:41:57 -0700

    Removed some commented-out code, and fixed a typo.

--- users.js
+++ users.js
@@ -17,8 +17,6 @@ function Tweet(id, text, date) {
   this.date = date;
 }
 
-//var vietTweets = [new Tweet(1, 'Yay camp is over', new Date('November 17, 1995 04:45:17')), new Tweet(2, Faker.Lorem.sentence(), new Date('March 19, 2016 09:15:01')), new Tweet(3, 'OMG so awkard', new Date(2016, 3, 29, 3, 24, 55))];
-
 var vietTweets = [new Tweet(0, 'OMG so awkard', Faker.Date.recent(50))];
 makeTweets(vietTweets, 1, 50);
 
@@ -28,7 +26,7 @@ makeTweets(benTweets, 1, 50);
 var nathanTweets = [new Tweet(0, 'This API sux', Faker.Date.recent(50))];
 makeTweets(nathanTweets, 1, 50);
 
-var schlomoTweets = [new Tweet(0, 'Nathan is wrong'), Faker.Date.recent(50))];
+var schlomoTweets = [new Tweet(0, 'Nathan is wrong', Faker.Date.recent(50))];
 makeTweets(schlomoTweets, 1, 50);
 
 var viet = new User('Viet', 1, 'Camp was a lot different for me as a kid', 'viethle126', vietTweets, [], [], 'img/viet.png');
@@ -47,44 +45,5 @@ function makeTweets(user, id, num) {
     user.push(temp);
   }
 }
-
-// User.prototype.addFollower = function(follower) {
-//   this.followers.push(follower);
-// }
-// User.prototype.addFollowing = function(following) {
-//   this.following.push(following);
-// }
-//
-// viet.addFollower(ben);
-// viet.addFollower(nathan);
-// viet.addFollower(schlomo);
-//
-// ben.addFollower(viet);
-// ben.addFollower(nathan);
-// ben.addFollower(schlomo);
-//
-// nathan.addFollower(ben);
-// nathan.addFollower(viet);
-// nathan.addFollower(schlomo);
-//
-// schlomo.addFollower(ben);
-// schlomo.addFollower(nathan);
-// schlomo.addFollower(viet);
-//
-// viet.addFollowing(ben);
-// viet.addFollowing(nathan);
-// viet.addFollowing(schlomo);
-//
-// ben.addFollowing(viet);
-// ben.addFollowing(nathan);
-// ben.addFollowing(schlomo);
-//
-// nathan.addFollowing(ben);
-// nathan.addFollowing(viet);
-// nathan.addFollowing(schlomo);
-//
-// schlomo.addFollowing(ben);
-// schlomo.addFollowing(nathan);
-// schlomo.addFollowing(viet);
 
 module.exports = users;
