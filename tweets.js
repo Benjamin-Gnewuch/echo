b74813ee92e0e51c0e231ace75e8ed1c42bdc140
Benjamin Gnewuch <bgnewuch@yahoo.com>
2016-03-31 14:38:26 -0700

    #1 User can now view a timeline of all tweets.

--- tweets.js
+++ tweets.js
@@ -3,14 +3,14 @@ var tweetCollection = function tweets() {
     makeTweet('@skofman', "This is the first tweet", 'December 17, 1995 03:24:00'),
     makeTweet('@viethle126', "This is the second tweet", 'February 28, 2001 05:15:17'),
     makeTweet('@treezrppl2', "This is the third tweet", 'July 21, 2010 01:22:13'),
-    makeTweet('@viethle126', "Nonsense", 'July 21, 2010 01:22:13'),
-    makeTweet('@skofman', "Nonsense", 'July 21, 2010 01:22:13'),
-    makeTweet('@treezrppl2', "Nonsense", 'July 21, 2010 01:22:13'),
-    makeTweet('@treezrppl2', "Nonsense", 'July 21, 2010 01:22:13'),
-    makeTweet('@viethle126', "Nonsense", 'July 21, 2010 01:22:13'),
-    makeTweet('@skofman', "Nonsense", 'July 21, 2010 01:22:13'),
-    makeTweet('@skofman', "Nonsense", 'July 21, 2010 01:22:13'),
-    makeTweet('@treezrppl2', "Nonsense", 'July 21, 2010 01:22:13'),
+    makeTweet('@viethle126', "This is the fourth tweet", 'July 21, 2010 01:22:13'),
+    makeTweet('@skofman', "This is the fifth tweet", 'July 21, 2010 01:22:13'),
+    makeTweet('@treezrppl2', "This is the sixth tweet", 'July 21, 2010 01:22:13'),
+    makeTweet('@treezrppl2', "This is the seventh tweet", 'July 21, 2010 01:22:13'),
+    makeTweet('@viethle126', "This is the eighth tweet", 'July 21, 2010 01:22:13'),
+    makeTweet('@skofman', "This is the ninth tweet", 'July 21, 2010 01:22:13'),
+    makeTweet('@skofman', "This is the tenth tweet", 'July 21, 2010 01:22:13'),
+    makeTweet('@treezrppl2', "This is the eleventh tweet", 'July 21, 2010 01:22:13'),
   ];
 
   function getTweets() {
